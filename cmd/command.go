b760ec4dfc3584de11ac855c21a5192e123b3cd5
nicholas frederich <nicholas.frederich.lagaunne@gmail.com>
2021-07-18 19:41:00 +0700

    Added cobra for CLI

---
+++ cmd/command.go
@@ -0,0 +1,24 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+func newCLI() *cobra.Command {
+	cli := &cobra.Command{
+		Use:   "cuan-tracker",
+		Short: "cuan-tracker",
+	}
+	cli.AddCommand(newServerCmd())
+	return cli
+}
+
+func newServerCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:     "server",
+		Short:   "start abyss-go server",
+		Aliases: []string{"server", "s"},
+		Run: func(_ *cobra.Command, _ []string) {
+			server.Start()
+		},
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module cuan-tracker
 go 1.16
 
 require (
-	github.com/go-pg/pg/v10 v10.10.2 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/go-pg/pg/v10 v10.10.2
+	github.com/spf13/cobra v1.2.1
+	github.com/stretchr/testify v1.7.0
+	gopkg.in/yaml.v2 v2.4.0
 )
